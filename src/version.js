ffcaf354e3a0a8108f15ee2752785225638ce258
Jonathan <wargus@wanadoo.fr>
2026-06-16 17:08:44 +0200

    Bump service worker to v2 pour forcer le rechargement du cache

--- src/version.js
+++ src/version.js
@@ -1,1 +1,1 @@
-export const BUILD_VERSION = '1';
+export const BUILD_VERSION = '2';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v2';
 const CACHE_NAME = 'reset-sos-' + CACHE_VERSION;
 
 self.addEventListener('install', (event) => {
